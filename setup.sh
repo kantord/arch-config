62301f94a688791e4f00008fad57a86f5e470194
Daniel Kantor <git@daniel-kantor.com>
2016-05-30 14:24:59 +0200

    Added more fonts

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-yaourt -S compton rofi zsh prezto-git ttf-font-icons
+yaourt -S compton rofi zsh prezto-git ttf-font-icons ttf-font-awesome input-font ttf-ms-fonts ttf-vista-fonts ttf-mplus
 cp .xinitrc ~
 cp -r .i3 ~
 cd i3

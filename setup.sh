b9fae2de7df3963b4e4d11aa99e62cb26f8ee615
Daniel Kantor <git@daniel-kantor.com>
2016-05-31 13:53:22 +0200

    Added gnome-software

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-yaourt -S compton rofi zsh ttf-font-icons ttf-font-awesome ttf-ms-fonts ttf-vista-fonts ttf-mplus gvim nautilus feh git-cola tig terminator python-pip
+yaourt -S compton rofi zsh ttf-font-icons ttf-font-awesome ttf-ms-fonts ttf-vista-fonts ttf-mplus gvim nautilus feh git-cola tig terminator python-pip gnome-software
 cp .xinitrc ~
 cp -r .i3 ~
 ln -s ~/repos/arch-config/.vimrc ~

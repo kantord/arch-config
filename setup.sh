afa854fa57fde074f34f9feb2bb4a16a256a46b1
Daniel Kantor <git@daniel-kantor.com>
2016-05-31 09:31:37 +0200

    Added tig

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-yaourt -S compton rofi zsh ttf-font-icons ttf-font-awesome ttf-ms-fonts ttf-vista-fonts ttf-mplus gvim nautilus feh git-cola
+yaourt -S compton rofi zsh ttf-font-icons ttf-font-awesome ttf-ms-fonts ttf-vista-fonts ttf-mplus gvim nautilus feh git-cola tig
 cp .xinitrc ~
 cp -r .i3 ~
 cd i3

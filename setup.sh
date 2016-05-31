d30413a0e66bca6cec146d66a041c8ac9b4d72e6
Daniel Kantor <git@daniel-kantor.com>
2016-05-31 11:18:31 +0200

    Temporarily removed i3 building
    This should be done manually

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 yaourt -S compton rofi zsh ttf-font-icons ttf-font-awesome ttf-ms-fonts ttf-vista-fonts ttf-mplus gvim nautilus feh git-cola tig terminator
 cp .xinitrc ~
 cp -r .i3 ~
-cd i3
-make install
-cd -
 ln -s ~/repos/arch-config/.vimrc ~
 ln -s ~/repos/arch-config/terminator ~/.config/terminator

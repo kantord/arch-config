18122af4e70757fdb99d9d82d99a3995cd27d103
Daniel Kantor <git@daniel-kantor.com>
2016-05-31 13:25:37 +0200

    Added pip

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-yaourt -S compton rofi zsh ttf-font-icons ttf-font-awesome ttf-ms-fonts ttf-vista-fonts ttf-mplus gvim nautilus feh git-cola tig terminator
+yaourt -S compton rofi zsh ttf-font-icons ttf-font-awesome ttf-ms-fonts ttf-vista-fonts ttf-mplus gvim nautilus feh git-cola tig terminator python-pip
 cp .xinitrc ~
 cp -r .i3 ~
 ln -s ~/repos/arch-config/.vimrc ~

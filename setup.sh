7a007112af9e177a95dafb789c745ea06207d219
Daniel Kantor <git@daniel-kantor.com>
2016-05-31 10:36:56 +0200

    Added vundle

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@ cp .xinitrc ~
 cp -r .i3 ~
 ln -s ~/repos/arch-config/.vimrc ~
 ln -s ~/repos/arch-config/terminator ~/.config/terminator
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall

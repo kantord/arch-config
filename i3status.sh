7b1dce9139643186420e9567f0d5779f1d77a1f6
Dániel Kántor <git@daniel-kantor.com>
2017-01-08 12:48:44 +0100

    Added pomodoro tools

--- i3status.sh
+++ i3status.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-i3status -c ~/.i3/i3status.conf
+i3status -c ~/.i3/i3status.conf | while :
+do
+    read line
+    pomodoro=`python ~/repos/i3-gnome-pomodoro/pomodoro-client.py status`
+    echo "$pomodoro| $line" || exit 1
+done

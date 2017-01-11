1ccb1b25c54ca449b59ecb2c3427ad7cf2cc9ab3
Dániel Kántor <git@daniel-kantor.com>
2017-01-11 21:58:48 +0100

    Some layout loading automation

---
+++ start_layout.sh
@@ -0,0 +1,10 @@
+# 1: plan
+gnome-calendar &
+urxvt -title "urxvt-planner" -cd "/home/kdani/plan" -hold -e nvim effort.md > /dev/null &
+i3-msg "workspace 1; append_layout ~/repos/arch-config/layout/1-plan.json";
+i3-msg "rename workspace 1 to 1: plan";
+
+# 2: track
+google-chrome-stable --app=https://habitica.com &
+i3-msg "workspace 2; append_layout ~/repos/arch-config/layout/2-track.json";
+i3-msg "rename workspace 2 to 2: track";
